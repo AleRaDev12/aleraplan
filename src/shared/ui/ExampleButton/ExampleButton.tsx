b54036441a1d51457db27549d572a2f77a59216e
aleradev12 <aleradev12@gmail.com>
2025-06-08 19:28:00 +0300

    feat: add TextArea shared component and remove unused ExampleButton

--- src/shared/ui/ExampleButton/ExampleButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Button } from "react-native-paper";
-
-export const ExampleButton = () => (
-  <Button icon="camera" mode="contained" onPress={() => console.log("Pressed")}>
-    Press me
-  </Button>
-);
